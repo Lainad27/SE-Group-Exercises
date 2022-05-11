2c14e18b72164c546a71f9532b941227562d9521
Lainad27 <66922386+Lainad27@users.noreply.github.com>
2022-05-11 14:48:44 +0300

    Update App.java

--- Ex2/App.java
+++ Ex2/App.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 import ex2.MobilePhone.AppPermissionsData;
 
-public abstract class App {
-	static Scanner input = new Scanner(System.in);
+public abstract class App { //All apps will inherit this class
+	static Scanner input = new Scanner(System.in); //The app needs a scanner for input
 	
-	public abstract void printOptions();
-	public abstract void handleCommand(String command);
-	public abstract String getAppName();
-	public abstract void init(AppPermissionsData data);
+	public abstract void printOptions(); //prints all command options
+	public abstract void handleCommand(String command); //Given a command, the app decides what to do.
+	public abstract String getAppName(); // Return's the name of the app.
+	public abstract void init(AppPermissionsData data); // This function will be called right after the app is started, so the app gets the latest data (like the phonebook)
 	
-	public void run(AppPermissionsData data) {
+	public void run(AppPermissionsData data) { //This function details how the app runs (taking commands)
 		
-		init(data);
+		init(data); // use latest data from phone
 		
 		System.out.println("Entered " + getAppName() + "!");
 		boolean programEnded = false;
