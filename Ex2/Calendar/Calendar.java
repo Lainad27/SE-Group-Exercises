c830881969f0e37fa9d44bd9c201230b0e2f0d01
Lainad27 <66922386+Lainad27@users.noreply.github.com>
2022-05-18 22:39:55 +0300

    Update Calendar.java

--- Ex2/Calendar/Calendar.java
+++ Ex2/Calendar/Calendar.java
@@ -40,7 +40,7 @@ public class Calendar {
         System.out.println("Event deleted.");
     }
 	
-	public class DateComparator implements Comparator<Event> { //class which compares 2 events
+	public class DateComparator implements Comparator<Event> { //class which compares 2 events by starting time
 	    public int compare(Event ev1, Event ev2)
         {
             return new Long(ev1.getDate().getTime()).compareTo(ev2.getDate().getTime());
@@ -57,14 +57,14 @@ public class Calendar {
     		System.out.println("There are no events on that day.");
     		return;
     	}
-        Collections.sort(sortedByTime, new DateComparator());
+        Collections.sort(sortedByTime, new DateComparator()); //sort by starting time
         for (Event events : sortedByTime)
             events.print();
     }
 
     public void printByContact(String name){ //print all meetings with contact
         ArrayList<Event> sortedByName= new ArrayList<Event>();
-        for (Event event : calendar) {
+        for (Event event : calendar) { //iterate over all events
             if (event.getContact() != null && event.getContact().getName().compareTo(name) == 0)
             	sortedByName.add(event);
         }
@@ -72,7 +72,7 @@ public class Calendar {
     		System.out.println("There are no events with that contact name.");
     		return;
     	}
-        Collections.sort(sortedByName, new DateComparator());
+        Collections.sort(sortedByName, new DateComparator());//sort by starting time
         for (Event events : sortedByName)
             events.print();
     }
@@ -85,34 +85,34 @@ public class Calendar {
             event.print();
     }
 
-	public void CheckdeletedContacts(PhoneBook phoneBook) {
+	public void CheckdeletedContacts(PhoneBook phoneBook) { //when starting the app, this function is called to check if all the events still have valid contacts
 		Iterator<Event> it = calendar.iterator();
 		while (it.hasNext()) {
 			Event ev = it.next();
-			if(ev.getContact()!= null && phoneBook.searchContact(ev.getContact().getName()) == null)
-				it.remove();
+			if(ev.getContact()!= null && phoneBook.searchContact(ev.getContact().getName()) == null) //if an event has a contact that is no longer in the phonebook
+				it.remove(); //delete it
 				
 		}
 		
 	}
 	
 	public void deleteOverlaps(){ //delete overlaped events
-		if (calendar.isEmpty()) {
+		if (calendar.isEmpty()) { //first check if empty
 			System.out.println("The calendar is empty - nothing to delete");
 			return;
 		}
+		Collections.sort(calendar, new DateComparator()); //sort by starting time
 		Iterator<Event> deleteIt = calendar.iterator();
 		
-		Event ev = deleteIt.next();
+		Event ev = deleteIt.next(); //save first event in ev
+		long lowerBound = ev.getDate().getTime() + ev.getMeetingDuration(); //get its "lower bound" - the earliest time the next event is allowed to start at
 		while (deleteIt.hasNext()) {
+			ev = deleteIt.next(); //get the next event
 			
-			long lowerBound = ev.getDate().getTime() + ev.getMeetingDuration();
-			ev = deleteIt.next();
-			
-			if (ev.getDate().getTime() < lowerBound)
-				deleteIt.remove();
-				if (deleteIt.hasNext())
-					ev = deleteIt.next();
+			if (ev.getDate().getTime() < lowerBound) //if it is overlapping
+				deleteIt.remove(); //delete it
+			else //if it is not, update the lower bound to its lower bound
+				lowerBound = ev.getDate().getTime() + ev.getMeetingDuration();
 		}
 		System.out.println("Overlaps deleted.");
     }
