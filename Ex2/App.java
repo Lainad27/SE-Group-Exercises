3276e9df488b084541ebe4268e0f6f110e7086e7
Lainad27 <66922386+Lainad27@users.noreply.github.com>
2022-05-18 22:43:35 +0300

    Update App.java

--- Ex2/App.java
+++ Ex2/App.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 import ex2.MobilePhone.AppPermissionsData;
 
-public abstract class App {
-	public static Scanner input = new Scanner(System.in);
-	public abstract String getOptions();
-	public void printOptions() {
+public abstract class App { //all apps will extend this class
+	public static Scanner input = new Scanner(System.in); //all apps need a scanner
+	public abstract String getOptions(); // this is a string with all the app options
+	public void printOptions() { // the final option string will also need to include the leave command
 		System.out.println(getOptions());
 		System.out.println("leave - leave the app");
 	}
-	public abstract void handleCommand(String command);
-	public abstract String getAppName();
-	public abstract void init(AppPermissionsData data);
+	public abstract void handleCommand(String command); // apps needs to handle commands
+	public abstract String getAppName(); 
+	public abstract void init(AppPermissionsData data); //this function is called when the app is opened
 	
-	public void run(AppPermissionsData data) {
+	public void run(AppPermissionsData data) { //this is how we run an app
 		
-		init(data);
+		init(data); // run function on input data - for example the phonebook maybe updated and we need to modify data accordingly
 		
 		System.out.println("Entered " + getAppName() + "!");
 		boolean programEnded = false;
