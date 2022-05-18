ad26e44d29cae00a61436c4c4c97e55b7adf92ec
ShonShafir <104515745+ShonShafir@users.noreply.github.com>
2022-05-18 13:52:32 +0300

    Add files via upload

--- Ex2/SMSApp.java
+++ Ex2/SMSApp.java
@@ -5,18 +5,18 @@ package ex2;
 import ex2.MobilePhone.AppPermissionsData;
 import ex2.MobilePhone.Permissions;
 
-public class SMSApp extends App {
+public class SMSApp extends App { 
 	
 	public String getAppName() {
 		return "SMS app";
 	}
 	
 	PhoneBook phoneBook;
-	SMSDatabase Data = new SMSDatabase();
+	SMSDatabase database = new SMSDatabase(); //load data - including all chats
 
 	public void printOptions() {
 		// TODO Auto-generated method stub
-		System.out.println("1 - Add chatting to contact (you can have no more than one chat with a contact).");
+		System.out.println("1 - Add chat to contact (you can have no more than one chat with a contact).");
 		System.out.println("2 - Delete chatting with contact.");
 		System.out.println("3 - Show chatting with chosen contact.");
 		System.out.println("4 - Find sentence in all chats (return list of all contacts, having at least one instance of the sentence in chat with them).");
@@ -28,80 +28,84 @@ public class SMSApp extends App {
 		switch (command) {
 		case "1":
 			System.out.println("Choose a Contact to chat with from the following contacts:");
-			phoneBook.printAllContacts();
-			String name = input.next();
-			Contact contact = phoneBook.searchcontact(name);
-			if (contact == null) {
-				System.out.println("No such contact exists. Create the contact first");
+			phoneBook.printAllContacts(); //show to user all options to choose from
+			String name = input.next();//gets contact name
+			Contact contact = phoneBook.searchcontact(name);//find contact by contact's name corresponding to phoneBook 
+			if (contact == null) { 
+				System.out.println("No such contact exists. Create the contact first"); //in case that contact not found in phoneBook 
 			}
 			else {
 				System.out.print("Enter The Message: ");
-				String message = input.next();
-				if(Data.IsContactInData(contact)) {
-					Data.returnChatByContact(contact).addMessage(message);
+				String message = input.next(); //gets message to add to chat
+				if(database.IsContactInData(contact)) {
+					database.returnChatByContact(contact).addMessage(message); //add message to chat with contact
 				}
-				else {
+				else { //in case that contact exist in phoneBook but not in Data (no chat exists with the contact)
 					Chat chat = new Chat(contact);
-					chat.addMessage(message);
-					Data.addChat(chat);
+					chat.addMessage(message);//add message to chat with contact
+					database.addChat(chat);//add the new chat to data
 				}
 			}		
 			break;
 			
 		case "2":
 			System.out.println("Choose a Contact to delete chat with from the following contacts");
-			phoneBook.printAllContacts();
-			String cntName = input.next();
-			Contact cntContact = phoneBook.searchcontact(cntName);
-			if (Data.IsContactInData(cntContact)) {
-				Chat cntChat = Data.returnChatByContact(cntContact);
-				Data.removeChat(cntChat);
+			phoneBook.printAllContacts();//show to user all options to choose from
+			String cntName = input.next();//gets contact name
+			Contact cntContact = phoneBook.searchcontact(cntName);//find contact by contact's name corresponding to phoneBook
+			if (database.IsContactInData(cntContact)) {
+				Chat cntChat = database.returnChatByContact(cntContact);//return chat by given contact
+				database.removeChat(cntChat);//delete chat with the contact
 			}
 			else {
-				System.out.println("No such contact exists. Create the contact first");
+				System.out.println("No such contact exists. Create the contact first");//in case that contact not found in phoneBook
 			}
 			
 			break;
 			
 		case "3":
-			System.out.println("Choose a Contact to chat with from the following contacts:");
-			phoneBook.printAllContacts();
-			String cntName3 = input.next();
-			Contact contact3 = phoneBook.searchcontact(cntName3);
-			if (contact3 == null) {
-				System.out.println("No such contact exists. Create the contact first");
+			if(!database.isEmpty()) {
+				System.out.println("Choose a Contact to print their messages from the following contacts:");
+				database.showAllContacts();//show to user all options to choose from
+				String cntName3 = input.next();//gets contact name
+				Contact contact3 = phoneBook.searchcontact(cntName3);//find contact by contact's name corresponding to phoneBook
+				if (contact3 == null) {
+					System.out.println("No such contact exists. Create the contact first");//in case that contact not found in phoneBook
+				}
+				else {
+					if(database.IsContactInData(contact3)) {
+						System.out.println(database.returnChatByContact(contact3).ChatToString());//print chat by given contact
+					}
+					else {
+						System.out.println("Add chat with contact first");//in case that contact exist in phoneBook but not in Data (no chat exists with the contact)
+					}
+				}
 			}
 			else {
-				System.out.println(Data.returnChatByContact(contact3).ChatToString());
+				System.out.println("No SMS chat found. Please create one first");//in case that data is empty
 			}
+			
 			break;
 			
 		case "4":
 			System.out.println("Please insert what sentence you want to find");
-			String sentence = input.next();
-			for(Chat chat:Data.getData()) {
-				if(chat.getMessages().contains(sentence)) {
-					System.out.println(chat.getContact().getName());
-				}
-			}
+			String sentence = input.next();//get sentence
+			System.out.println(database.printContactsbysentence(sentence));//return all contact names that include a given sentence in their chats
 			break;
 		
-		case "5":
-			
-			for (Chat chat:Data.getData()) {
-				System.out.println(chat.ChatToString());
-				System.out.print("\n");
-			}
+		case "5":			
+			database.showAllChats();//print all chats
 			break;
 			
 		default:
-			System.out.println("Invalid command. Please enter \"h\" to see command options.");
+			System.out.println("Invalid command. Please enter \"h\" to see command options.");//invalid command
 			break;
 		} 	
 	}
 	
 	public void init(AppPermissionsData data) {
-		phoneBook = (PhoneBook) data.getDataFromPerm(Permissions.PHONEBOOK);
+		phoneBook = (PhoneBook) data.getDataFromPerm(Permissions.PHONEBOOK); //phoneBook of the App updates to main phone phoneBook, each time the user opens the App 
+		database.CheckdeletedContacts(phoneBook);//delete chats corresponding to deleted contacts in the main phone phoneBook
 		
 	}
 }
--- Ex2/SMSDatabase.java
+++ Ex2/SMSDatabase.java
@@ -2,41 +2,69 @@ package ex2;
 
 import java.util.*;
 
-public class SMSDatabase {
-	private HashSet<Chat> Data = new HashSet<Chat>();
+public class SMSDatabase {  // this is where all chats are stored
+	private HashSet<Chat> data;
 	
-	public HashSet<Chat> getData() {
-		return this.Data;
+	
+	public SMSDatabase() {
+		this.data = new HashSet<Chat>(); //we chose HashSet database because the order of chats is not important and unique 
 	}
 	
-	public void addChat(Chat chat) {
-		Data.add(chat);
+	public HashSet<Chat> getData() { // get all chats
+		return this.data;
 	}
 	
-	public void removeChat(Chat chat) {
-		Data.remove(chat);
+	public void addChat(Chat chat) { // add chat
+		data.add(chat);
+	}
+	
+	public void removeChat(Chat chat) { //remove chat
+		data.remove(chat);
+	}
+	
+	public boolean isEmpty() { //return true if data is empty
+		if(data.isEmpty())
+			return true;
+		else
+			return false;
 	}
 		
-	public void showAllContacts() {
-		if(!Data.isEmpty()) {
-			Iterator<Chat> it = Data.iterator();
+	public void showAllContacts() { //show all contacts corresponding to chats
+		if(!data.isEmpty()) {
+			Iterator<Chat> it = data.iterator();
 			while (it.hasNext()) {
 				Chat i = it.next();
-				System.out.print(i.getContact().getName() + " ");
+				System.out.println(i.getContact().getName());
 			}
-			System.out.println("\n");
+			System.out.print("\n");
+		}
+		else {
+			System.out.println("No Contacts added in App");
+		}
+	}
+	
+	public void showAllChats() { //print all chats
+		if(!data.isEmpty()) {
+			Iterator<Chat> it = data.iterator();
+			while (it.hasNext()) {
+				Chat i = it.next();
+				System.out.println(i.getContact().getName());
+				System.out.println(i.ChatToString());
+			}
+			System.out.print("\n");
 		}
 		else {
 			System.out.println("The Chat list is empty");
 		}
 	}
 	
-	public boolean IsContactInData(Contact contact) {
-		if(Data.isEmpty()) {
+	
+	public boolean IsContactInData(Contact contact) { //return true if exists a chat corresponding to given contact
+		if(data.isEmpty()) {
 			return false;
 		}
 		else {
-			Iterator<Chat> it = Data.iterator();
+			Iterator<Chat> it = data.iterator();
 			while (it.hasNext()) {
 				Chat i = it.next();
 				if(contact == i.getContact()) {
@@ -47,8 +75,8 @@ public class SMSDatabase {
 		return false;
 	}
 	
-	public Chat returnChatByContact(Contact contact) {
-		Iterator<Chat> it = Data.iterator();
+	public Chat returnChatByContact(Contact contact) { //return chat corresponding to given contact
+		Iterator<Chat> it = data.iterator();		
 		while (it.hasNext()) {
 			Chat i = it.next();
 			if(contact == i.getContact()) {
@@ -57,4 +85,26 @@ public class SMSDatabase {
 		}
 		return null;
 	}
+	
+    public String printContactsbysentence(String sentence) { //return all contact names that include a given sentence in their chats
+    	String res = "";
+    	for(Chat chat:data) {
+			if(chat.getMessages().contains(sentence)) {
+				res = res.concat(chat.getContact().getName());
+				res = res.concat("\n");
+			}
+		}
+    	return res;
+    }
+    
+    public void CheckdeletedContacts(PhoneBook phoneBook) { //remove chats of deleted accounts in the PhoneBook
+    	
+		Iterator<Chat> it = data.iterator();
+		while (it.hasNext()) {
+			Chat i = it.next();
+			if(phoneBook.searchcontact(i.getContact().getName()) == null)
+				data.remove(i);
+				
+		}
+    }
 }
